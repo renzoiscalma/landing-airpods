af6595d00f82b3202b3956d99d4795680ef2d60b
Renzo Calma <xeganthy@gmail.com>
2023-08-14 00:12:57 -0300

    Added custom plugins for mask image, added keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -9,7 +11,42 @@ module.exports = {
       colors: {
         "code-green": "#00ff41",
       },
+      keyframes: {
+        "airpods-connecting": {
+          "0%": {
+            backgroundColor: "#9d9d9d",
+          },
+          "38.46%": {
+            backgroundColor: "#fff",
+          },
+          "50%": {
+            backgroundColor: "#fff",
+          },
+          "88.46%": {
+            backgroundColor: "#9d9d9d",
+          },
+          to: {
+            backgroundColor: "#9d9d9d",
+          },
+        },
+      },
+      animation: {
+        "airpods-connecting": "airpods-connecting 1.3s infinite",
+      },
     },
   },
-  plugins: ["prettier-plugin-tailwindcss"],
+  // opening directly on html won't work with url you need a static server to have cross origin
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities({
+        "mask-img": (value) => {
+          return {
+            "mask-image": `${value}`,
+            "-webkit-mask-image": `${value}`,
+          };
+        },
+      });
+    }),
+  ],
 };
